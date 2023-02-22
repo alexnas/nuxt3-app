9a3fb863b9a11070877850a988320a57987ca845
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-22 19:45:31 +0300

    Fetch and show todos list from JsonPlaceholder server

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
+<script>
+import { defineNuxtComponent } from '#app';
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: [],
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then((response) => response.json())
+        .then((json) => {
+          this.todoList = json;
+        });
+    },
+  },
+});
+</script>
+
 <template>
   <div>
+    <img src="/todo.jpg" alt="Todo photo by Glenn Carstens-Peters" />
     <h1>Hello Frontend on the Nuxt!</h1>
+    <button @click="fetchTodoList">Fetch Todo List</button>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" />
+        {{ todo.title }}
+      </li>
+    </ul>
+    <pre>
+      {{ todoList }}
+    </pre>
   </div>
 </template>
