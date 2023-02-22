dbadfdcc7fa4707a2a4d8aa79e6ec2c7cf16eff1
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-22 22:03:45 +0300

    Fetch and show list of photos in PhotoGalery

--- app.vue
+++ app.vue
@@ -3,13 +3,26 @@ import { defineNuxtComponent } from '#app';
 export default defineNuxtComponent({
   data: () => ({
     todoList: [],
+    photoGallery: [],
   }),
+  computed: {
+    numberOfPhotos() {
+      return this.photoGallery.length;
+    },
+  },
   methods: {
     fetchTodoList() {
       fetch('https://jsonplaceholder.typicode.com/todos/')
         .then((response) => response.json())
         .then((json) => {
           this.todoList = json;
+        });
+    },
+    fetchPhotos() {
+      fetch('https://jsonplaceholder.typicode.com/photos/')
+        .then((response) => response.json())
+        .then((json) => {
+          this.photoGallery = json;
         });
     },
   },
@@ -20,6 +33,14 @@ export default defineNuxtComponent({
   <div>
     <img src="/todo.jpg" alt="Todo photo by Glenn Carstens-Peters" />
     <h1>Hello Frontend on the Nuxt!</h1>
+    <button type="button" @click.prevent="fetchPhotos">Fetch photosList</button>
+    <span> Whole set: {{ numberOfPhotos }} photos</span>
+    <ul>
+      <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
+        <img :src="`${photo.thumbnailUrl}`" :alt="photo.title" />
+        {{ photo.title }}
+      </li>
+    </ul>
     <button @click="fetchTodoList">Fetch Todo List</button>
     <ul>
       <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
