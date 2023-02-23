27fa682f8b4a1d2acc3c9db256c9bf72bb3ab7fc
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-23 18:56:20 +0300

    Remake component using Composition API

--- app.vue
+++ app.vue
@@ -1,32 +1,66 @@
-<script>
-import { defineNuxtComponent } from '#app';
-export default defineNuxtComponent({
-  data: () => ({
-    todoList: [],
-    photoGallery: [],
-  }),
-  computed: {
-    numberOfPhotos() {
-      return this.photoGallery.length;
-    },
-  },
-  methods: {
-    fetchTodoList() {
-      fetch('https://jsonplaceholder.typicode.com/todos/')
-        .then((response) => response.json())
-        .then((json) => {
-          this.todoList = json;
-        });
-    },
-    fetchPhotos() {
-      fetch('https://jsonplaceholder.typicode.com/photos/')
-        .then((response) => response.json())
-        .then((json) => {
-          this.photoGallery = json;
-        });
-    },
-  },
+<script setup>
+import { ref, computed } from 'vue';
+
+let photoGallery = ref([]);
+
+const numberOfPhotos = computed(() => {
+  return photoGallery.value.length;
 });
+
+function fetchPhotos() {
+  console.log('fetch Photos');
+  fetch('https://jsonplaceholder.typicode.com/photos/')
+    .then((response) => response.json())
+    .then((json) => {
+      photoGallery.value = json;
+    });
+}
+
+let todoList = ref([]);
+const numberOfTodos = computed(() => {
+  return todoList.value.length;
+});
+
+const numberOfCompletedTodos = computed(() => {
+  return todoList.value.filter((todo) => todo.completed).length;
+});
+
+function fetchTodoList() {
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then((response) => response.json())
+    .then((json) => {
+      todoList.value = json;
+    });
+}
+
+// import { defineNuxtComponent } from '#app';
+// export default defineNuxtComponent({
+//   data: () => ({
+//     todoList: [],
+//     photoGallery: [],
+//   }),
+//   computed: {
+//     numberOfPhotos() {
+//       return this.photoGallery.length;
+//     },
+//   },
+//   methods: {
+//     fetchTodoList() {
+//       fetch('https://jsonplaceholder.typicode.com/todos/')
+//         .then((response) => response.json())
+//         .then((json) => {
+//           this.todoList = json;
+//         });
+//     },
+//     fetchPhotos() {
+//       fetch('https://jsonplaceholder.typicode.com/photos/')
+//         .then((response) => response.json())
+//         .then((json) => {
+//           this.photoGallery = json;
+//         });
+//     },
+//   },
+// });
 </script>
 
 <template>
@@ -34,14 +68,17 @@ export default defineNuxtComponent({
     <img src="/todo.jpg" alt="Todo photo by Glenn Carstens-Peters" />
     <h1>Hello Frontend on the Nuxt!</h1>
     <button type="button" @click.prevent="fetchPhotos">Fetch photosList</button>
-    <span> Whole set: {{ numberOfPhotos }} photos</span>
+    <p>Whole set: {{ numberOfPhotos }} photos</p>
     <ul>
       <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
         <img :src="`${photo.thumbnailUrl}`" :alt="photo.title" />
         {{ photo.title }}
       </li>
     </ul>
+    <hr />
     <button @click="fetchTodoList">Fetch Todo List</button>
+    <p>Whole set: {{ numberOfTodos }} todos</p>
+    <p>Completed: {{ numberOfCompletedTodos }} todos</p>
     <ul>
       <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
         <input type="checkbox" :checked="todo.completed" />
