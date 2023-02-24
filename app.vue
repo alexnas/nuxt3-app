8dcd891bf253cd4c334bbbbf4962ef8ad32ed048
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-24 12:32:00 +0300

    Use Bulma for styling

--- app.vue
+++ app.vue
@@ -35,36 +35,54 @@ function fetchTodoList() {
 </script>
 
 <template>
-  <div>
-    <img src="/todo.jpg" alt="Todo photo by Glenn Carstens-Peters" />
-    <h1>Hello Frontend on the Nuxt!</h1>
-    <button type="button" @click.prevent="fetchPhotos">Fetch photosList</button>
-    <p>Whole set: {{ numberOfPhotos }} photos</p>
-    <ul>
-      <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
-        <img :src="`${photo.thumbnailUrl}`" :alt="photo.title" />
-        {{ photo.title }}
-      </li>
-    </ul>
-    <hr />
-    <button @click="fetchTodoList">Fetch Todo List</button>
-    <p>Whole set: {{ numberOfTodos }} todos</p>
-    <p>Completed: {{ numberOfCompletedTodos }} todos</p>
-    <ul :class="$style.list">
-      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
-        <input type="checkbox" :checked="todo.completed" />
-        {{ todo.title }}
-      </li>
-    </ul>
-    <pre>
-      {{ todoList }}
-    </pre>
+  <div class="container">
+    <div class="section">
+      <img src="/todo.jpg" alt="Todo photo by Glenn Carstens-Peters" />
+      <h1 class="title">Hello Frontend on the Nuxt!</h1>
+      <button type="button" @click.prevent="fetchPhotos">Fetch photosList</button>
+      <p>Whole set: {{ numberOfPhotos }} photos</p>
+      <ul class="photo-galery-list">
+        <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
+          <img :src="`${photo.thumbnailUrl}`" :alt="photo.title" />
+          <!-- {{ photo.title }} -->
+        </li>
+      </ul>
+      <hr />
+    </div>
+    <div>
+      <button @click="fetchTodoList">Fetch Todo List</button>
+      <p>Whole set: {{ numberOfTodos }} todos</p>
+      <p>Completed: {{ numberOfCompletedTodos }} todos</p>
+      <ul class="list">
+        <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+          <input type="checkbox" :checked="todo.completed" />
+          {{ todo.title }}
+        </li>
+      </ul>
+      <pre>{{ todoList }}</pre>
+    </div>
   </div>
 </template>
 
-<style module>
+<style lang="scss">
+@import './node_modules/bulma/bulma.sass';
+@import './assets/styles/main.scss';
+
+:root {
+  --text-color: #{$textColor};
+}
+.heading {
+  color: var(--text-color);
+}
+
 .list {
+  color: var(--text-color);
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+}
+
+.photo-galery-list {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
 }
 </style>
