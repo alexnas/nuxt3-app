24d0805976d2f759c232a62e2f95d495c93e0cf1
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-28 11:00:33 +0300

    Create folder structure for automatic routing; create Navbar

---
+++ components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <NuxtLink to="/" class="navbar-item">My Recs</NuxtLink>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start"></div>
+
+      <div class="navbar-end">
+        <NuxtLink to="/" class="navbar-item"> Home </NuxtLink>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <NuxtLink to="#" class="navbar-link"> Display </NuxtLink>
+
+          <div class="navbar-dropdown">
+            <NuxtLink to="/display" class="navbar-item"> Todos </NuxtLink>
+            <NuxtLink to="/display" class="navbar-item"> Photo Gallery </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ pages/display.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <div class="section">
+      <div class="columns">
+        <div class="column">
+          <TodoViewer />
+        </div>
+        <div class="column">
+          <PhotoGallery />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
