14f30f6c5aac5ea339cfc2988e6bc3aa71145bdc
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-28 21:24:45 +0300

    Add photo details page with dynamic routing from the list

--- pages/display/photos.vue
+++ pages/display/photos.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, computed } from 'vue';
+const route = useRoute();
 
 const props = defineProps({
   title: {
@@ -16,15 +17,20 @@ const totally = computed(() => {
 </script>
 
 <template>
-  <BaseDisplay :title="title" v-model:itemList="itemList">
-    <template v-slot:metrics>
-      <p>Totally: {{ totally.length }}</p>
-    </template>
-    <template v-slot:items>
-      <li v-for="item in itemList" :key="item.id">
-        <img :src="item.thumbnailUrl" :alt="item.title" />
-        {{ item.title.slice(1, 10) }}...
-      </li>
-    </template>
-  </BaseDisplay>
+  <div class="section">
+    <NuxtPage v-if="route.params.id" />
+    <BaseDisplay v-else :title="title" v-model:itemList="itemList">
+      <template v-slot:metrics>
+        <p>Totally: {{ totally.length }}</p>
+      </template>
+      <template v-slot:items>
+        <li v-for="item in itemList" :key="item.id">
+          <NuxtLink :to="`/display/photos/${item.id}`">
+            <img :src="item.thumbnailUrl" :alt="item.title" />
+            {{ item.title.slice(1, 10) }}...
+          </NuxtLink>
+        </li>
+      </template>
+    </BaseDisplay>
+  </div>
 </template>
---
+++ pages/display/photos/[id].vue
@@ -0,0 +1,8 @@
+<script setup>
+const route = useRoute();
+</script>
+
+<template>
+  <h1 class="title">Photo details</h1>
+  <h2 class="subtitle">This is photo id {{ route.params.id }}</h2>
+</template>
