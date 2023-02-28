eef27fec4df047d1c7dbd627246932ab32ada0dd
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-28 13:10:47 +0300

    Transfer all the todos and photos logic into the proper pages to elimitate duplicates

--- pages/display/photos.vue
+++ pages/display/photos.vue
@@ -1,7 +1,34 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Photo Gallery',
+  },
+});
+
+const itemType = route.path.split('/').pop();
+const itemList = ref([]);
+
+const totally = computed(() => {
+  return itemList.value;
+});
+</script>
 
 <template>
-  <PhotoGallery />
+  <BaseDisplay :title="title" :itemType="itemType" v-model:itemList="itemList">
+    <template v-slot:metrics>
+      <p>Totally: {{ totally.length }}</p>
+    </template>
+    <template v-slot:items>
+      <li v-for="item in itemList" :key="item.id">
+        <img :src="item.thumbnailUrl" :alt="item.title" />
+        {{ item.title.slice(1, 10) }}...
+      </li>
+    </template>
+  </BaseDisplay>
 </template>
-
-<style lang="scss" scoped></style>
--- pages/display/todos.vue
+++ pages/display/todos.vue
@@ -1,7 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Todo List',
+  },
+});
+
+const itemType = route.path.split('/').pop();
+const itemList = ref([]);
+
+const totally = computed(() => {
+  return itemList.value;
+});
+
+const completed = computed(() => {
+  return itemList.value.filter((item) => item.completed);
+});
+</script>
 
 <template>
-  <TodoViewer />
+  <BaseDisplay :title="title" :itemType="itemType" v-model:itemList="itemList">
+    <template v-slot:metrics>
+      <p>Totally: {{ totally.length }} || Completed: {{ completed.length }}</p>
+    </template>
+    <template v-slot:items>
+      <li v-for="item in itemList" :key="item.id">
+        <input type="checkbox" :checked="item.completed" />
+        <span>{{ item.title }}</span>
+      </li>
+    </template>
+  </BaseDisplay>
 </template>
-
-<style lang="scss" scoped></style>
